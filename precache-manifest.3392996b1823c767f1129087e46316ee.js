53c6e6c6d987ce8d6ceb7382e4c80d919b0c50d0
NafisIrtija <irtija_nafis@hotmail.com>
2020-10-16 15:58:23 -0600

    Updates

---
+++ precache-manifest.3392996b1823c767f1129087e46316ee.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9840edb86a3f5f56b341d4825831f9cf",
+    "url": "/REFRESH/index.html"
+  },
+  {
+    "revision": "33a9b14c9ab3394ba362",
+    "url": "/REFRESH/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "cc5f0bea7f919b9ed1a8",
+    "url": "/REFRESH/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "33a9b14c9ab3394ba362",
+    "url": "/REFRESH/static/js/2.141e6bdc.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/REFRESH/static/js/2.141e6bdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc5f0bea7f919b9ed1a8",
+    "url": "/REFRESH/static/js/main.539b0487.chunk.js"
+  },
+  {
+    "revision": "17b5259de141608fa42b",
+    "url": "/REFRESH/static/js/runtime-main.4f69fb7b.js"
+  },
+  {
+    "revision": "4169105ad6f3675260ac1ce5dd1b817e",
+    "url": "/REFRESH/static/media/desert.4169105a.jpg"
+  }
+]);
